06d1946ec41702fc75a048f6d1496a826482b760
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-11-29 15:39:28 +0100

    home page basic ok

--- hubris/urls.py
+++ hubris/urls.py
@@ -18,9 +18,11 @@ from django.urls import path
 from django.urls.conf import include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("bestiary/", include("bestiary.urls")),
     path("markdownx/", include("markdownx.urls")),
+    path("", TemplateView.as_view(template_name="index.html"), name="index"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
