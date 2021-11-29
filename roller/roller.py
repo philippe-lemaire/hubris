2cbd6d5dfcf8e9d31032bc997c8f9ba7548cc3e8
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-11-29 17:19:05 +0100

    roller object to make dice rolls

---
+++ roller/roller.py
@@ -0,0 +1,13 @@
+import random
+
+
+class Roller:
+    def roll(self, dice):
+        if "+" in dice:
+            d, mod = dice.split("+")
+            d = int(d[1:])
+            mod = int(mod)
+        else:
+            d = int(dice[1:])
+            mod = 0
+        return random.randint(1, d) + mod
