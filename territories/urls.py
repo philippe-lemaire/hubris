9293285ad90e939b895946386bc3315c9009d7c1
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-11-30 10:47:36 +0100

    now with the map

--- territories/urls.py
+++ territories/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 app_name = "territories"
@@ -9,4 +12,4 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("<slug:slug>", views.TerritoryDetailView.as_view(), name="territory"),
     path("locations/<slug:slug>", views.LocationDetailView.as_view(), name="location"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
